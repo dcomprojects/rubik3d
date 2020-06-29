c8aa728d840c7cafd6117e97b39da8d473c96f09
Dave O'Mahony <david.c.omahony@gmail.com>
2020-06-29 14:37:36 -0230

    mobile height issue #8

--- app/js/main.js
+++ app/js/main.js
@@ -40,6 +40,13 @@ onload().then(() => {
     // Then we set the value in the --vh custom property to the root of the document
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+    // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
     window.globalShow = () => {
         console.log("Hello");
     };
