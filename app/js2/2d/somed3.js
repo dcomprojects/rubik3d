2e2759ffbfc3b5b8cfb1f127fd543110d32b8875
Dave O'Mahony <david.c.omahony@gmail.com>
2020-07-26 20:04:28 -0230

    layout tweaks

--- app/js2/2d/somed3.js
+++ app/js2/2d/somed3.js
@@ -19,11 +19,13 @@ function buildData(inData) {
 
 function drawCube(width, height, inData) {
 
-    let dim = d3.min([width, height]);
+    let dim = d3.min([width, height]) * 0.9;
 
     const svg = d3.create("svg")
         .attr("viewBox", [0, 0, dim, dim])
-        .attr("height", "100%");
+        .attr("height", dim);
+
+    let cdim = (dim/3.0);
 
 
     let pieceGroup = svg.append("g")
@@ -35,24 +37,25 @@ function drawCube(width, height, inData) {
             g.append("path")
                 .attr("d", d => {
                     let p = d3.path();
-                    p.rect(d[0] * dim / 3.0, d[1] * dim / 3.0, dim / 3.0, dim / 3.0);
+                    //p.rect(d[0] * dim / 3.0, d[1] * dim / 3.0, dim / 3.0, dim / 3.0);
+                    p.rect(d[0] * cdim, d[1] * cdim, cdim, cdim);
                     return p;
                 })
                 .attr("fill", d => d[2]);
 
             g.append("line")
                 .attr("stroke", "black")
-                .attr("x1", d => d[0] * dim / 3.0)
-                .attr("x2", d => (d[0] + 1) * dim / 3.0)
-                .attr("y1", d => d[1] * dim / 3.0)
-                .attr("y2", d => (d[1]) * dim / 3.0);
+                .attr("x1", d => d[0] * cdim)
+                .attr("x2", d => (d[0] + 1) * cdim)
+                .attr("y1", d => d[1] * cdim)
+                .attr("y2", d => (d[1]) * cdim);
 
             g.append("line")
                 .attr("stroke", "black")
-                .attr("x1", d => d[0] * dim / 3.0)
-                .attr("x2", d => (d[0]) * dim / 3.0)
-                .attr("y1", d => d[1] * dim / 3.0)
-                .attr("y2", d => (d[1] + 1) * dim / 3.0);
+                .attr("x1", d => d[0] * cdim)
+                .attr("x2", d => (d[0]) * cdim)
+                .attr("y1", d => d[1] * cdim)
+                .attr("y2", d => (d[1] + 1) * cdim);
 
         });
 
